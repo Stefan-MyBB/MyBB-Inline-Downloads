e7128cf3ca205c8f7ae200d90be5e8952d3620de
Stefan-ST <544140+Stefan-MyBB@users.noreply.github.com>
2011-01-02 18:54:51 +0100

    Fixed path

---
+++ upload/inc/languages/deutsch_sie/downloads.lang.php
@@ -0,0 +1,73 @@
+<?php
+ /**
+ * This file is part of Download-System for MyBB.
+ * Copyright (C) 2006-2011 StefanT (http://www.mybbcoder.info)
+ * https://github.com/Stefan-ST/MyBB-Inline-Downloads
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+$l['ds_title'] = 'Titel';
+$l['ds_version'] = 'Version';
+$l['ds_dateline'] = 'Hochgeladen';
+$l['ds_downloads'] = 'Downloads';
+$l['ds_size'] = 'Größe';
+$l['ds_demo'] = 'Vorschau';
+$l['ds_link'] = 'extern';
+$l['ds_download'] = 'Herunterladen';
+$l['ds_original'] = 'Original';
+$l['ds_source'] = 'Quelle';
+$l['ds_license'] = 'Lizenz';
+$l['ds_author'] = 'Autor';
+$l['ds_author_website'] = 'Autoren-Website';
+$l['ds_author_original'] = 'Original';
+$l['ds_description'] = 'Beschreibung';
+
+$l['ds_file'] = 'Datei';
+
+$l['ds_accept'] = "Annehmen";
+$l['ds_not_accept'] = "Ablehnen";
+
+$l['ds_opt_templates'] = "Ändert Templates";
+$l['ds_opt_files'] = "Ändert Dateien";
+$l['ds_opt_db'] = "Ändert Datenbank";
+$l['ds_guestdl_yes'] = "Gäste können downloaden";
+$l['ds_guestdl_no'] = "Gäste können nicht downloaden";
+
+$l['ds_error_use_button'] = "Bitte benutzen Sie den Download-Button!";
+$l['ds_error_is_download'] = "Sie können dieses Thema nicht benutzen. Es ist ein Download.";
+
+$l['ds_information'] = "Download-Informationen";
+$l['ds_please_select'] = "-Bitte auswählen-";
+$l['ds_file_link'] = "Link";
+$l['ds_link_thread'] = "Download-Link";
+$l['ds_edit_time'] = "Als neue Version markieren";
+
+$l['ds_optional'] = 'Optional';
+
+$l['ds_own'] = "Sind Sie der Autor";
+$l['ds_forum'] = 'Forum';
+
+$l['postdata_ds_no_title'] = "Sie haben keinen Titel angegeben.";
+$l['postdata_ds_no_description'] = "Sie haben keine Beschreibung angegeben.";
+$l['postdata_ds_no_type'] = "Sie haben keinen Typ angegeben.";
+$l['postdata_ds_no_file'] = "Sie haben keine Datei angegeben.";
+$l['postdata_ds_no_version'] = "Sie haben keine Version angegeben.";
+$l['postdata_ds_no_author'] = "Sie haben keinen Autoren angegeben.";
+$l['postdata_ds_no_link'] = "Sie haben keinen Download-Link angegeben.";
+
+$l['ds_license_text'] = 'Das Copyright liegt beim Autor. Der Autor/Übersetzer haftet nicht für Schäden jeglicher Art, sollten Ihnen diese durch die Verwendung des Downloads entstehen.';
+
+?>
